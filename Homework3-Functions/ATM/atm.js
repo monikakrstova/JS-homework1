2b52e4b4a899b532e20cf27b32d8ce620fefc459
monikakrstova <119454573+monikakrstova@users.noreply.github.com>
2022-12-18 13:59:09 +0100

    Update atm.js

--- Homework3-Functions/ATM/atm.js
+++ Homework3-Functions/ATM/atm.js
@@ -1,17 +1,42 @@
+//function atm() {
+//   var withdraw;
+//   withdraw = parseInt(
+//     prompt(`Please enter the amount you want to withdrawn:`)
+//   );
+//   let availableBalance = 26000;
+//   let moneyLeft = availableBalance - withdrawn;
+//   if (withdraw !== "" && withdraw !== null && !isNAN(withdraw));
+//     if (withdraw >= 26000) {
+//     console.log("Not enough money.");
+//     } else if (withdraw <= 2600) {
+//     console.log(
+//       `Your withdrawn amount is ${withdraw}, and your available balance is ${moneyLeft(availableBalance)}.`
+//     );
+//     } else {
+//     console.log(`Thank you for using our ATM.`)
+//   } {
+//   console.log(`ERROR, Please try again.`)
+//   return moneyLeft;
+// }
 let availableBalance = 26000;
-let userInput = prompt(`Please enter the amount you want to withdrawn:`);
-let requestMoney = parseFloat(userInput);
-let moneyLeft;
-
-function atm (availableBalance=26000, requestMoney){
-    let moneyLeft = availableBalance - requestMoney;
-    return moneyLeft;
-
-}
-if (requestMoney>=26000) {
-    console.log("Not enough money.")
-}else if (requestMoney<=2600){
-    console.log(`Your withdrawn amount is ${requestMoney}, and your available balance is ${atm(moneyLeft)}.`)
-}else {
-    console.log(`ERROR, Please try again.`)
+function deposit () {
+  var depositAmount = parseInt(prompt("How much money do you want to deposit?"));
+  if (depositAmount !== "" && depositAmount !== null && !isNaN(depositAmount)) {
+    if (depositAmount >= 50) {
+      if (depositAmount <= availableBalance) {
+        currentBalance -= depositAmount;
+        alert("Transaction successful!");
+        alert("Your ramaining balance is $" +currentBalance);
+      } else {
+        alert("You do not have sufficient funds!");
+        depositAmount ()
+      } 
+    } else {
+      alert ("You must deposit at least $50");
+      depositAmount ();
+    }
+  } else {
+    alert ("ERRPR: please enter a number!")
+    depositAmount ();
+  }
 }
